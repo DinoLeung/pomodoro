ab013a4b59b75ee5eb9307c6da604f68bc35cbc2
Dino Leung <dino.leung@enabled.com.au>
2021-03-21 22:47:01 +1030

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -13,32 +13,30 @@ class _BaseAppState extends State<BaseApp> {
   String _title = 'Pomodoro Timer';
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      theme: ThemeData(
-          primarySwatch: Colors.lightGreen,
-          textTheme: GoogleFonts.ibmPlexSansTextTheme()),
-      home: Scaffold(
-        backgroundColor: Colors.redAccent[100],
-        appBar: AppBar(
-            title:
-                Image.asset('assets/image/tomato.png', width: 40, height: 40)),
-        body: Builder(
-          builder: (context) => Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(10),
-            child: FittedBox(
-              child: TimerWidget(
-                duration: Duration(minutes: 25),
-                tick: Duration(milliseconds: 250),
-                onTick: (String countdown) =>
-                    setState(() => _title = countdown),
+  Widget build(BuildContext context) => MaterialApp(
+        title: _title,
+        theme: ThemeData(
+            primarySwatch: Colors.lightGreen,
+            textTheme: GoogleFonts.ibmPlexSansTextTheme()),
+        home: Scaffold(
+          backgroundColor: Colors.redAccent[100],
+          appBar: AppBar(
+              title: Image.asset('assets/image/tomato.png',
+                  width: 40, height: 40)),
+          body: Builder(
+            builder: (context) => Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              child: FittedBox(
+                child: TimerWidget(
+                  duration: Duration(minutes: 25),
+                  tick: Duration(milliseconds: 250),
+                  onTick: (String countdown) =>
+                      setState(() => _title = countdown),
+                ),
               ),
             ),
           ),
         ),
-      ),
-    );
-  }
+      );
 }
